2b673627f044cfec7d00304e4e373a52eb93541b
Bhavish <111994995+bhavish95@users.noreply.github.com>
2023-06-12 18:13:16 +0530

    Create DiceGameProblem.java

---
+++ DiceGameProblem.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class DiceGameProblem {
+
+    static ArrayList<String> diceGame(int currentValue, int endValue){
+        if(currentValue == endValue){
+             // Part of the result (10 Reach)
+             ArrayList<String> result = new ArrayList<>();
+             result.add("");
+             return result;
+        }
+
+        if(currentValue>endValue){
+            // Exceed 10 (Not Part of the result)
+            ArrayList<String> result  = new ArrayList<>();
+            return result;
+        }
+       
+        
+        // 1 to 6 Calls
+        // Roll the Dice (Can be 1 to 6)
+        ArrayList<String> finalResult = new ArrayList<>();
+        for(int dice = 1; dice<=6; dice++){
+            int newValue = currentValue + dice;
+            ArrayList<String> list = diceGame(newValue, endValue);
+            for(String s : list){
+                finalResult.add(dice + s);
+            }
+        }
+        return finalResult;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> result = diceGame(0,10);
+        System.out.println(result);
+    }
+}
